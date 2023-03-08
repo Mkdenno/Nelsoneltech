16e8d2f87bd0745aa2bda2a29598dffa1430a713
Mkdenno <denniskiprop25@gmail.com>
2023-03-08 09:01:19 +0300

    It will add about page

--- src/components/pages/Price.js
+++ src/components/pages/Price.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Price = () => {
   return (
-    <div>Price</div>
+    <div className='lg:flex justify-center items-center m-8 flex-wrap'>
+      <div className='pricediv1 lg:w-1/3 lg:ml-2 lg:mr-2 mt-8  '>
+
+
+      </div>
+        <div className='pricediv1 lg:w-1/3 lg:ml-2 lg:mr-2 mt-8  '>
+
+      </div>
+      <div className='pricediv1 lg:w-1/3 lg:ml-2 lg:mr-2  mt-8'>
+
+    </div>
+    </div>
   )
 }
 
